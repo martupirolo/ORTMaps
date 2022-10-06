b7dabaf319475b61c2050de0148dc240f387a145
martupirolo <111439509+martupirolo@users.noreply.github.com>
2022-10-06 09:48:35 -0300

    asda

---
+++ Mapa.API.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORTmaps.Apps
+{ 
+    public partial class Mapa : Form
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mapa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
